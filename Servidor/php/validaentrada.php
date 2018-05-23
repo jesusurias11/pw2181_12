00e49f222c6510f34866ffb5f8010027d6d53a3b
angel acosta <acosta1904@hotmail.com>
2018-05-23 12:53:26 -0600

    conexiones fallida2s

--- Servidor/php/validaentrada.php
+++ Servidor/php/validaentrada.php
@@ -1,28 +1,28 @@
-<?php
+<?php 
 include 'conexiones.php';
 function valida(){
-	respuesta = false;
+	$respuesta=false;
 	$usuario=$_POST["usuario"];
-	$clave=md5($_POST["clave"]);
-	//conectarnos al motor de BD
+	$clave	=md5($_POST["clave"]);
 	$con=conecta();
-	consulta = "select * from usuarios where usuario ='".$usuario."' and clave= '".$clave."'limit 1";
+	$consulta="select usuario,clave from usuarios where usuario= '".$usuario."' and clave='".$clave."' limit 1";
 	$resConsulta=mysqli_query($con,$consulta);
-	if(mysqli_num_rows($resConsulta)>0){
-		$respuesta=true;
-	}
-	$salidaJSON = array('respuesta' => $respuesta );
+	if(mysqli_num_rows($resConsulta)>0)
+	{
+	 	$respuesta=true;
+    }
+    $salidaJSON = array('respuesta' => $respuesta);
 	print json_encode($salidaJSON);
 }
-$opc=$_POST["opc"];
-switch ($opc) {
-	case 'validaentrada':
-		# code...
-	valida();
-		break;
 	
-	default:
-		# code...
-		break;
-}
+	
+	$opc=$_POST["opc"];
+	switch ($opc) {
+		case 'validaentrada':
+			valida();
+			break;
+		
+		default:
+			break;
+	}
 ?>
