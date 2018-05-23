a84c0fc253dcff161ce207dd141d7e10c90bf1b1
angel acosta <acosta1904@hotmail.com>
2018-05-23 12:45:51 -0600

    conexiones fallidas

--- Servidor/js/eventos.js
+++ Servidor/js/eventos.js
@@ -3,7 +3,7 @@ var Aceptar=function(){
 
 	var usuario=$("#txtUsuario").val();
 	var clave=$("#txtClave").val();
-	var parametros="opc=validaentrada"+&usuario+usuario+"&clave"+clave+"&aleatorio"+Math.random();
+	var parametros="opc=validaentrada"+"&usuario="+usuario+"&clave="+clave+"&aleatorio="+Math.random();
     
     $.ajax({
     	cache:false,
